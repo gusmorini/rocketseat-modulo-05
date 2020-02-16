fff045395439e0df1edef50fdd2c8ecb4dadbb29
Gustavo Morini <gustavommh@gmail.com>
2020-02-16 14:17:15 -0300

    list + local storage

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -59,6 +59,34 @@ export const SubmitButton = styled.button.attrs(({ loading }) => ({
   }
 `;
 
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+
+    flex-direction: now;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
+`;
+
 /*
+
   attrs() passa propriedades pro component
+
+  & + li aplica a estilização à partir do segundo elemento da lista, no caso li
+
+
 */
